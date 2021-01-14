3cb7cdc144639aa360fd44498cb72b45e678fff6
NAOY <72992708+NAOYY@users.noreply.github.com>
2021-01-14 18:00:44 +0100

    Update terminaltables.py

--- core/terminaltables.py
+++ core/terminaltables.py
@@ -23,5 +23,5 @@ def terminaltables(github_username):
 	[f'{getgithubResult["name"]}', f'{getgithubResult["location"]}', f'{getgithubResult["email"]}', f'{getgithubResult["bio"]}', f'{getgithubResult["twitter_username"]}', f'{getgithubResult["followers"]}', f'{getgithubResult["following"]}']
 	]
 
-	table = SingleTable(table_data, f'{github_username}')
+	table = SingleTable(table_data, f' {github_username} ')
 	print("\n" + table.table)
