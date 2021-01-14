8921de4addc269c8122d61683c883815b8dd69e9
NAOY <72992708+NAOYY@users.noreply.github.com>
2021-01-14 18:04:19 +0100

    Update Github-Scraping.py

--- Github-Scraping.py
+++ Github-Scraping.py
@@ -1,6 +1,8 @@
 from core.terminaltables import terminaltables
 from core.tree import tree
+from colorama import Fore, init
 
 github_username = input("Github Username: ")
+print(f"[{Fore.RED}!{Fore.RESET}] Scraping en cours sur '{github_username}'...")
 terminaltables(github_username)
 tree(github_username)
